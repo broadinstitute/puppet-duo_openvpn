f93c973feafa8004f6fde4ff58be40bf3217fcce
Andrew Teixeira <teixeira@broadinstitute.org>
2020-04-28 13:18:33 -0400

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,119 @@
+# Class: duo_openvpn
+# ===========================
+#
+# Download, build, and install the duo_openvpn OpenVPN plugin
+#
+# Parameters
+# ----------
+#
+# * `default_packages`
+# An array of packages to require (Default: curl, gcc, make)
+#
+# * `download_path`
+# The directory to which the tar file for duo_openvpn will be downloaded (Default: /tmp)
+#
+# * `download_url`
+# The URL from which duo_openvpn will be downloaded (Default: auto-generated from version)
+#
+# * `extract_path`
+# The path where the downloaded tar file will be extracted (Default: /opt/duo_openvpn)
+#
+# * `group`
+# The group ownership on the install path directory (Default: root)
+#
+# * `install_path`
+# The location where the built plugin will be installed (Default: /opt/duo)
+#
+# * `install_path_mode`
+# The directory mode (permissions) on the install path directory (Default: 0700)
+#
+# * `owner`
+# The owner on the install path directory (Default: root)
+#
+# * `version`
+# The version of the duo_openvpn plugin to install (Default: 2.4)
+#
+#
+# Examples
+# --------
+#
+# @example
+#  class { 'duo_openvpn': }
+#
+# @example
+#  class { 'duo_openvpn':
+#    install_path => '/usr/local/duo',
+#    version      => '2.0',
+#  }
+#
+# Authors
+# -------
+#
+# Broad Institute
+#
+# Copyright
+# ---------
+#
+# Copyright 2020 Broad Institute
+#
+class duo_openvpn (
+  Array $default_packages,
+  Stdlib::Absolutepath $download_path,
+  Optional[Stdlib::HTTPSUrl] $download_url,
+  Stdlib::Absolutepath $extract_path,
+  String $group,
+  Stdlib::Absolutepath $install_path,
+  String $install_path_mode,
+  Boolean $manage_packages,
+  String $owner,
+  String $version,
+) {
+
+  if ($download_url and $download_url != '') {
+    $archive_url = $download_url
+  } else {
+    $archive_url = "https://github.com/duosecurity/duo_openvpn/archive/${version}.tar.gz"
+  }
+
+  if $manage_packages {
+    package { $default_packages:
+      ensure => 'present',
+    }
+  }
+
+  file { 'duo_openvpn_path':
+    ensure => 'directory',
+    backup => false,
+    group  => $group,
+    mode   => $install_path_mode,
+    owner  => $owner,
+    path   => $install_path,
+  }
+
+  # All package installations happen before we use archive
+  Package<| |> -> Archive<| |>
+
+  archive { "${download_path}/duo_openvpn-${version}.tar.gz":
+    creates      => "${extract_path}/duo_openvpn-${version}",
+    extract      => true,
+    extract_path => $extract_path,
+    notify       => Exec['duo_openvpn_build'],
+    source       => $archive_url,
+  }
+
+  exec { 'duo_openvpn_build':
+    command     => 'make',
+    cwd         => "${extract_path}/duo_openvpn-${version}",
+    path        => '/usr/sbin:/usr/bin:/sbin:/bin',
+    refreshonly => true,
+    notify      => Exec['duo_openvpn_install'],
+  }
+
+  exec { 'duo_openvpn_install':
+    command     => "make PREFIX=${install_path} install",
+    cwd         => "${extract_path}/duo_openvpn-${version}",
+    path        => '/usr/sbin:/usr/bin:/sbin:/bin',
+    refreshonly => true,
+    require     => File['duo_openvpn_path'],
+  }
+}
